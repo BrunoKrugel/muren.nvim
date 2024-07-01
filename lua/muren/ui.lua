1262dfcde74d937e078b274930c4fdf63dee659c
Bruno Krugel <brunokrugel@gmail.com>
2024-07-01 19:08:03 -0300

    nui

--- lua/muren/ui.lua
+++ lua/muren/ui.lua
@@ -13,54 +13,6 @@ local last_lines = {}
 local last_edited_bufs
 local last_undoed_bufs
 
-local popup_options = {
-  relative = "cursor",
-  position = {
-    row = 2,
-    col = 2,
-  },
-  border = {
-    style = "rounded",
-    highlight = "FloatBorder",
-    text = {
-      top = "[Replace]",
-      top_align = "center",
-    },
-  },
-  highlight = "Normal:Normal",
-}
-
-local move_screen = function()
-  local screen = vim.opt.lines:get()
-  local redraw = [[
-    normal! %s
-    redraw
-  ]]
-
-  if screen > 13 and screen < 26 then
-    vim.cmd(redraw:format "zt")
-  else
-    vim.cmd(redraw:format "zz")
-  end
-end
-
-local get_row = function()
-  local cursor_line = vim.fn.line "."
-  local first_line = vim.fn.line "w0"
-  local fold = { up = -7, down = 2 }
-
-  local height = vim.fn.winheight(0)
-  local diff_start = cursor_line - first_line
-
-  local remaining_space = height - (diff_start + 1)
-
-  if remaining_space < 9 then
-    return fold.up
-  end
-
-  return fold.down
-end
-
 -- TODO should these be global to the module?
 local is_open = false
 local preview_open = false
@@ -423,49 +375,54 @@ local align_cursor = function(master_win)
 end
 
 local do_replace = function()
-  move_screen()
-  popup_options.position.row = get_row()
+  local Menu = require "nui.menu"
+  local event = require("nui.utils.autocmd").event
 
-  local menu = Menu(popup_options, {
+  local menu = Menu({
+    position = "50%",
+    size = {
+      width = 25,
+      height = 5,
+    },
+    border = {
+      style = "rounded",
+      text = {
+        top = "Confirm replacement?",
+        top_align = "center",
+      },
+    },
+    win_options = {
+      winhighlight = "Normal:Normal,FloatBorder:Normal",
+    },
+  }, {
     lines = {
-      Menu.item("* Yes", { action = "replace" }),
-      Menu.item("* No", { action = "quit" }),
+      Menu.item "Yes (y)",
+      Menu.item "No (n)",
     },
     max_width = 20,
-    separator = {
-      char = "─",
-      text_align = "center",
-    },
     keymap = {
       focus_next = { "j", "<Down>", "<Tab>" },
       focus_prev = { "k", "<Up>", "<S-Tab>" },
       close = { "<Esc>", "<C-c>" },
-      submit = { "<CR>" },
+      submit = { "<CR>", "<Space>" },
     },
     on_close = function()
-      vim.notify "Replacement canceled"
+      print "Menu Closed!"
     end,
     on_submit = function(item)
-      local lines = get_ui_lines()
-      last_edited_bufs = search.do_replace_with_patterns(lines.patterns, lines.replacements, options.values)
-      last_undoed_bufs = nil
-
-      -- Notify user that the replacement was done
-      vim.notify "Replacement done successfully"
+      print("Menu Submitted: ", item.text)
+      if item.text == "Yes (y)" then
+        local lines = get_ui_lines()
+        last_edited_bufs = search.do_replace_with_patterns(lines.patterns, lines.replacements, options.values)
+        last_undoed_bufs = nil
+        vim.notify "Replacement done successfully"
+      else
+        print "No"
+      end
     end,
   })
 
   menu:mount()
-
-  local function map(lhs, rhs)
-    vim.api.nvim_buf_set_keymap(menu.bufnr, "n", lhs, rhs, { noremap = false, nowait = true })
-  end
-
-  map("y", "gg<CR>")
-  map("n", "2gg<CR>")
-  map("a", "4gg<CR>")
-  map("q", "5gg<CR>")
-  map("l", "6gg<CR>")
 end
 
 local do_undo = function()
